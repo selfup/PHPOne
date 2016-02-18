de09238307fc099785b66ad2f162db8219f71d2f
selfup <boudinot.regis@yahoo.com>
2016-02-18 13:37:50 -0700

    using classes

--- index.php
+++ index.php
@@ -9,8 +9,21 @@
   <body>
 
     <?php
+      
+      class Demo {
+        public $insOne = "Something";
+        public $insTwo = "Something Else";
+
+        function outputIns() {
+          echo $this->insOne;
+        }
+      }
+
+      $d = new Demo;
 
       echo "<h1>My first PHP script!</h1>";
+
+      $d->outputIns();
 
       for ($x = 0; $x <= 10; $x++) {
         echo "<h3>The number is: $x <h1>";
