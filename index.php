87f5756fca93c8f1e3dcb41da9590def4a23f14e
selfup <boudinot.regis@yahoo.com>
2016-02-18 13:50:18 -0700

    Fun format

--- index.php
+++ index.php
@@ -15,18 +15,18 @@
         public $insTwo = "Something Else";
 
         function outputIns() {
-          echo $this->insOne;
+          echo "<h2><em>$this->insOne and $this->insTwo</em></h2><hr>";
         }
       }
 
       $d = new Demo;
 
-      echo "<h1>My first PHP script!</h1>";
-
+      echo "<h1>Class output and then a for loop</h1><hr>";
+      
       $d->outputIns();
-
+     
       for ($x = 0; $x <= 10; $x++) {
-        echo "<h3>The number is: $x <h1>";
+        echo "<h3>The number is: $x </h3><hr>";
       }
 
     ?>
