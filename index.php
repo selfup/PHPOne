b2c7f1dc6e53344ca2bb674d8b8734c25452a0b3
selfup <boudinot.regis@yahoo.com>
2016-02-18 10:10:20 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      PHPOne
+    </title>
+  </head>
+
+  <body>
+
+    <?php
+
+      echo "<h1>My first PHP script!</h1>";
+
+      for ($x = 0; $x <= 10; $x++) {
+        echo "<h3>The number is: $x <h1>";
+      }
+
+    ?>
+
+  </body>
+</html>
